0cf9e197d36cb9194dc8367683f636cf49457116
Irfan Noor <37165903+IrfanNoor@users.noreply.github.com>
2018-05-28 11:01:49 -0700

    Add files via upload

---
+++ Notepad/Notepad/About.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Notepad
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(506, 397);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 39;
+            this.lineShape1.X2 = 432;
+            this.lineShape1.Y1 = 122;
+            this.lineShape1.Y2 = 122;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 105);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "This product is licence under the ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.linkLabel1.Location = new System.Drawing.Point(328, 292);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(77, 15);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Irfan Noor";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "user :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(132, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "&OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(443, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 397);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.About_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ Notepad/Notepad/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filename = "";
+        private System.Drawing.Printing.PrintDocument docToPrint = new System.Drawing.Printing.PrintDocument();
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult click = MessageBox.Show("The text in the Untitled has changed.\n\n Do you want to save the changes?", " My Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                if (click == DialogResult.Yes)
+                {
+                    if (filename == "")
+                    {
+                        saveFileDialog1.Filter = "Text Files|*.txt";
+                        DialogResult result = saveFileDialog1.ShowDialog();
+                        if (result == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+                        filename = saveFileDialog1.FileName;
+                        // MessageBox.Show(fname);
+                    }
+                    StreamWriter write = new StreamWriter(filename);
+                    write.WriteLine(textBox1.Text);
+                    write.Flush();
+                    //  textBox1.Text = "";
+                    write.Close();
+
+                    textBox1.Text = "";
+                    filename = "";
+                    // bool flag = false;
+                }
+                if (click == DialogResult.No)
+                {
+                    textBox1.Clear();
+                    this.Text = "Untitled-Notepad";
+                }
+            }
+            else
+            {
+                textBox1.Clear();
+                filename = "";
+                this.Text = "Untitled-Notepad";
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    string str = openFileDialog1.FileName, str2;
+                    FileStream fS = new FileStream(str, FileMode.Open, FileAccess.Read);
+                    StreamReader sr = new StreamReader(fS);
+                    str2 = sr.ReadToEnd();
+                    textBox1.Text = str2;
+                    sr.Close();
+                    fS.Close();
+                    this.Text = openFileDialog1.FileName ;
+                }
+                else
+                {
+                    MessageBox.Show(" Not Opened File");
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+            {
+                saveFileDialog1.Filter = "Text Files|*.txt";
+                DialogResult result = saveFileDialog1.ShowDialog();
+                if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+                filename = saveFileDialog1.FileName;
+                StreamWriter s = new StreamWriter(filename);
+                s.WriteLine(textBox1.Text);
+                s.Flush();
+                s.Close();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Files|*.txt";
+            saveFileDialog1.ShowDialog();
+            filename = saveFileDialog1.FileName;
+            if (filename == "")
+            {
+                saveFileDialog1.Filter = "Text Files|*.txt";
+                DialogResult result = saveFileDialog1.ShowDialog();
+                if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+                filename = saveFileDialog1.FileName;
+            }
+            StreamWriter s = new StreamWriter(filename);
+            s.WriteLine(textBox1.Text);
+            s.Flush();
+            s.Close();
+        }
+
+        private void pageSetupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.PageSettings = new System.Drawing.Printing.PageSettings();
+            pageSetupDialog1.PrinterSettings = new System.Drawing.Printing.PrinterSettings();
+            pageSetupDialog1.ShowNetwork = false;
+            DialogResult result = pageSetupDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                object[] results = new object[]{
+                    pageSetupDialog1.PageSettings.Margins,
+                    pageSetupDialog1.PageSettings.PaperSize,
+                    pageSetupDialog1.PageSettings.Landscape,
+                    pageSetupDialog1.PrinterSettings.PrinterName,
+                    pageSetupDialog1.PrinterSettings.PrintRange};
+                //richTextBox1.Text.LastIndexOf(results);
+            }
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*printDialog1.AllowSomePages = true;
+            printDialog1.ShowHelp = true;
+            printDialog1.Document = docToPrint;
+            DialogResult result = printDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                docToPrint.Print();
+            }*/
+            if (printPreviewDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult click = MessageBox.Show("The text in the Untitled has changed.\n\n Do you want to save the changes?", " My Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                if (click == DialogResult.Yes)
+                {
+                    if (filename == "")
+                    {
+                        saveFileDialog1.Filter = "Text Files|*.txt";
+                        DialogResult result = saveFileDialog1.ShowDialog();
+                        if (result == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+                        filename = saveFileDialog1.FileName;
+                        // MessageBox.Show(fname);
+                    }
+                    StreamWriter write = new StreamWriter(filename);
+                    write.WriteLine(textBox1.Text);
+                    write.Flush();
+                    write.Close();
+                    Application.Exit();
+                }
+                if (click == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {           
+               if (textBox1.CanUndo)
+                    textBox1.Undo();          
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.SelectedText != "")
+            {
+                cutToolStripMenuItem.Enabled = true;
+                textBox1.Cut();
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.SelectedText != "")
+            {
+                textBox1.Copy();
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.SelectedText = "";
+            }
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //FindTab tab = new FindTab(this);
+            //tab.Show();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.SelectAll();
+            }
+            else
+            {
+                MessageBox.Show("No Text is there");
+            }
+        }
+
+        private void timeDateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string timeDate;
+            timeDate = DateTime.Now.ToShortTimeString() + " " +
+            DateTime.Now.ToShortDateString();
+            int newSelectionStart = textBox1.SelectionStart + timeDate.Length;
+            textBox1.Text = textBox1.Text.Insert(textBox1.SelectionStart, timeDate);
+            textBox1.SelectionStart = newSelectionStart;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+            fontDialog1.ShowDialog();
+            textBox1.Font = fontDialog1.Font;
+            textBox1.ForeColor = fontDialog1.Color;
+            Properties.Settings.Default.font = textBox1.Font;
+            Properties.Settings.Default.color = textBox1.ForeColor;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Font = Properties.Settings.Default.font;
+            textBox1.ForeColor = Properties.Settings.Default.color;
+            textBox1.Focus();
+            this.Text = "Untitled-Notepad";
+        }
+
+        private void wordToolStripMenuItem_Click(object sender, EventArgs e)
+        {         
+           if( wordToolStripMenuItem.CheckState==CheckState.Checked)
+            {                
+                textBox1.Multiline = true;
+                textBox1.ScrollBars = ScrollBars.Vertical;
+                textBox1.AcceptsReturn = true;
+                textBox1.AcceptsTab = true;
+                textBox1.WordWrap = true;
+            }
+           else
+            {             
+                textBox1.Multiline = true;
+                textBox1.ScrollBars = ScrollBars.Horizontal;
+                textBox1.AcceptsReturn = false;
+                textBox1.AcceptsTab = false;
+                textBox1.WordWrap = false;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+            {
+                undoToolStripMenuItem.Enabled = true;
+                cutToolStripMenuItem.Enabled = true;
+                copyToolStripMenuItem.Enabled = true;
+                deleteToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                undoToolStripMenuItem.Enabled = false;
+                cutToolStripMenuItem.Enabled = false;
+                copyToolStripMenuItem.Enabled = false;
+                deleteToolStripMenuItem.Enabled = false;
+            }
+            undoToolStripMenuItem.Enabled = true;
+            status.Text = "Line: " + (textBox1.GetLineFromCharIndex(Int32.MaxValue) + 1) + "   Cols: " + textBox1.Text.Length;
+
+        }
+
+        private void contentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://go.microsoft.com/fwlink/?LinkId=834783");
+        }
+
+        private void indexToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About a = new About();
+            a.Show();
+        }
+
+        private void statusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show( "Line: " + (textBox1.GetLineFromCharIndex(Int32.MaxValue) + 1) + "   Cols: " + textBox1.Text.Length);
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(textBox1.Text, new Font("Times New Roman", 14, FontStyle.Bold), Brushes.Black, new PointF(100, 100));
+        }
+    }
+
+}
